092f6cc3d5c07d820aa6d12b5ec50c0e12289dd4
Ansel Adams <dj@volaciousbrands.com>
2018-12-29 23:02:09 -0600

    remove old files

--- client/src/components/LoggedOut/LoggedOut.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const LoggedOut = () =>
-  <div className="jumbotron">
-    Logged Out
-  </div>;
-
-export default LoggedOut;
--- client/src/components/VehicleItem/VehicleItem.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export const VehicleItem = props => (
-  <li className="list-group-item">
-    {props.children}
-  </li>
-);
