a6428bc002bf10a5b010c930f11f7b264bfde28a
DJ Steinmetz <dsteinmetz@four51.com>
2019-07-16 16:49:09 -0500

    :lipstick: begin nav styling

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -3,56 +3,14 @@ import "../../css/style.css";
 
 const Nav = props => {
   return (
-    // <nav className="flex items-center justify-between flex-wrap bg-gray-200 p-4">
-    //   <a href="/">Stat.US</a>
-    //   <div class="block lg:hidden">
-    //     <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
-    //       <svg
-    //         class="fill-current h-3 w-3"
-    //         viewBox="0 0 20 20"
-    //         xmlns="http://www.w3.org/2000/svg"
-    //       >
-    //         <title>Menu</title>
-    //         <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-    //       </svg>
-    //     </button>
-    //   </div>
-    //   <div className="collapse navbar-collapse" id="navbarNavDropdown">
-    //     <ul className="navbar-nav">
-    //       <li className="nav-item active">
-    //         <a className="nav-link" href="/">
-    //           Home <span className="sr-only">(current)</span>
-    //         </a>
-    //       </li>
-    //       <li className="nav-item">
-    //         {props.isLoggedIn === false ? (
-    //           <a className="nav-link" href="/login">
-    //             Log-in
-    //           </a>
-    //         ) : (
-    //           <a className="nav-link" onClick={props.logout} href={null}>
-    //             Logout
-    //           </a>
-    //         )}
-    //       </li>
-    //       <li className="nav-item">
-    //         {props.isLoggedIn === false ? (
-    //           <a className="nav-link" href="/register">
-    //             Register
-    //           </a>
-    //         ) : null}
-    //       </li>
-    //     </ul>
-    //   </div>
-    // </nav>
-    <nav class="flex items-center justify-between flex-wrap bg-gray-300 p-6">
-      <div class="flex items-center flex-shrink-0 text-gray-900 mr-6">
+    <nav class="flex items-center justify-start flex-wrap bg-gray-800 px-6 py-4">
+      <div class="flex items-center flex-shrink-0 text-gray-100 mr-6">
         <a href="/" class="font-semibold text-xl tracking-tight">
           Stat.US
         </a>
       </div>
       <div class="block lg:hidden">
-        <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+        <button class="flex items-center px-3 py-2 border rounded text-gray-200 border-gray-400 hover:text-white hover:border-white">
           <svg
             class="fill-current h-3 w-3"
             viewBox="0 0 20 20"
@@ -66,18 +24,22 @@ const Nav = props => {
       <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
         <div class="text-sm lg:flex-grow">
           {props.isLoggedIn === false ? (
-            <a className="nav-link" href="/login">
+            <a className="nav-link cursor-pointer text-gray-200" href="/login">
               Log-in
             </a>
           ) : (
-            <a className="nav-link" onClick={props.logout} href={null}>
+            <a
+              className="nav-link cursor-pointer text-gray-200"
+              onClick={props.logout}
+              href={null}
+            >
               Logout
             </a>
           )}
         </div>
         <div className="text-sm lg:flex-grow">
           {props.isLoggedIn === false ? (
-            <a className="nav-link" href="/register">
+            <a className="nav-link text-gray-200" href="/register">
               Register
             </a>
           ) : null}
