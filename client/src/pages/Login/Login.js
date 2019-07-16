828407c7917846b47c077c463db6b3c3c8660ddf
DJ Steinmetz <dsteinmetz@four51.com>
2019-07-16 16:50:16 -0500

    :lipstick: style login page // inputs

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import { auth } from "../../firebase"
-import Nav from '../../components/Nav';
+import { auth } from "../../firebase";
+import Nav from "../../components/Nav";
 
 class Login extends Component {
   state = {
@@ -19,50 +19,79 @@ class Login extends Component {
           isLoggedIn: true,
           message: ""
         });
-        window.location = "/"
+        window.location = "/";
       })
       .catch(error => {
+        console.log(error);
         this.setState({ message: error.message });
       });
   };
   handleChange = e => {
     let { name, value } = e.target;
     this.setState({
-      [name]: value,
+      [name]: value
     });
   };
 
   render() {
     return (
       <React.Fragment>
-        <Nav
-          isLoggedIn={this.state.isLoggedIn}
-          logout={this.handleSignOut}
-        />
+        <Nav isLoggedIn={this.state.isLoggedIn} logout={this.handleSignOut} />
         <div className="container">
           <div className="row">
-            <div className="col-md-12 text-center">
+            <div className="col-md-12">
               <form onSubmit={this.handleSignIn}>
-                <div>
-                  <strong>Log in to your MNPUBDEF account.</strong>
-                </div>
-                <div >
-                  <input type="text" value={this.state.email} name="email" onChange={this.handleChange} placeholder="Email"></input>
-                  <input type="password" value={this.state.password} name="password" onChange={this.handleChange} placeholder="Password"></input>
-                  <div className="row">
-                    <div id="error" className="col-12 text-danger"></div>
-                  </div>
-                </div>
-                <div className="modal-footer">
-                  <button type="button" className="btn btn-light" onClick={this.handleCloseModal} data-dismiss="modal">Close</button>
-                  <button className="btn btn-light" type="submit">Sign In</button>
+                <h1 class="text-xl font-bold text-center mt-4">
+                  Log in to your Stat.US account
+                </h1>
+                <div className="max-w-md m-auto mt-3">
+                  <label
+                    class="block text-gray-500 text-l text-sm font-bold mb-1"
+                    for="email"
+                  >
+                    Email
+                  </label>
+                  <input
+                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    value={this.state.email}
+                    name="email"
+                    onChange={this.handleChange}
+                    id="email"
+                    type="text"
+                    placeholder="Email"
+                  />
+                  <label
+                    class="block text-gray-500 text-sm font-bold mb-1 mt-4"
+                    for="password"
+                  >
+                    Password
+                  </label>
+                  <input
+                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    type="password"
+                    value={this.state.password}
+                    name="password"
+                    onChange={this.handleChange}
+                    placeholder="*********************"
+                  />
+                  {this.state.message && (
+                    <p className="text-xs italic text-red-600">
+                      {this.state.message}
+                    </p>
+                  )}
+                  <button
+                    className="px-4 py-2 bg-pink-200 font-uppercase font-bold text-sm rounded-sm mt-4"
+                    type="submit"
+                  >
+                    Sign In
+                  </button>
                 </div>
               </form>
             </div>
           </div>
         </div>
       </React.Fragment>
-    )
+    );
   }
 }
 
